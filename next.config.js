60886394578fd717bedc774b8322abbf59668e0d
Kohta9521 <kohta0421kochi@gmail.com>
2023-09-18 00:06:31 +0900

    feat(.github/workflows/nextjs.yml next.config.js package.json): nextjsをgithub pagesでdeployする
    github actions用のymlファイルの設定とpackage.json内にdeployy用の記述を追加した

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  },
+});

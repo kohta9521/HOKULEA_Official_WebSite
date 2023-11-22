79bb72de649dc8b07188a0af973bacdc0f03aeaa
k_95.21 <kohta0421kochi@gmail.com>
2023-11-23 00:15:24 +0900

    add new dir constracture

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// scss import
+import styles from './About.module.scss'
+
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default About

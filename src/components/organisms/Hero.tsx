1c7fa96905f998141b1c66b573e289e4d792f506
k_95.21 <kohta0421kochi@gmail.com>
2023-11-24 20:19:43 +0900

    add scrolltextbox

--- src/components/organisms/Hero.tsx
+++ src/components/organisms/Hero.tsx
@@ -5,13 +5,14 @@ import styles from "./styles/Hero.module.scss";
 
 // components import
 import ScrollBox from "../atoms/ScrollBox";
+import ScrollText from "../atoms/ScrollText";
 
 const Hero = () => {
   return (
     <div className={styles.hero}>
       <div className={styles.container}>
         <h1>this area is hero</h1>
-        {/* TODO: 絶対位置でbottom: -40px; 固定にする */}
+        <ScrollText />
         <ScrollBox />
       </div>
     </div>

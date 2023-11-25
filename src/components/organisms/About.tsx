d7ae3d721728eca7f69c4bcf2167c7c7dda16ccf
k_95.21 <kohta0421kochi@gmail.com>
2023-11-25 18:25:36 +0900

    add new components about moreButton and newSectio about pc layouts

--- src/components/organisms/About.tsx
+++ src/components/organisms/About.tsx
@@ -5,6 +5,7 @@ import styles from "./styles/About.module.scss";
 
 // components import
 import SecTitle from "../atoms/SecTitle";
+import ReadMoreButton from "../atoms/ReadMoreButton";
 
 const About = () => {
   return (
@@ -16,6 +17,22 @@ const About = () => {
           jaTitle={"『HOKULEA』とは"}
           enTitle={"ABOUT"}
         />
+        <div className={styles.flexBox}>
+          <div className={styles.leftBox}>
+            <img className={styles.image} src="/images/about.jpg" alt="aboutImage" />
+          </div>
+          <div className={styles.rightBox}>
+            <h1 className={styles.title}>Community of Generation Z</h1>
+            <p className={styles.text}>
+              若者のクリエイティブとエナジーが行き交う場。<br />
+              今までにない若者のコミュニティがここにはある。<br />
+              そんな彼らと「魅せる」と「楽しむ」をモットーに様々なジャンルのイベントを作り上げます。<br />
+            </p>
+            <div className={styles.buttonBox}>
+              <ReadMoreButton id={1} link="/about" text="READ MORE" textColor="white" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

0e5786c7c37f3629b59e254e7aad4094dff8b1dd
k_95.21 <kohta0421kochi@gmail.com>
2023-11-28 09:49:34 +0900

    add contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,43 @@
 import React from "react";
 
 // scss import
+import styles from './Contact.module.scss'
+
+// components import
+import Header from "@/components/organisms/Header";
+import SubPageHeroLayout from "@/components/templates/SubPageHeroLayout";
+import ScrollText from "@/components/atoms/ScrollText";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className={styles.contact}>
+      <Header />
+      <SubPageHeroLayout
+        id={7}
+        linklevel={
+          [
+            {
+                id: 1,
+                text: "HOME",
+                link: "/"
+            },
+            {
+                id: 2,
+                text: "COMPANY",
+                link: "/company"
+            }
+          ]
+        }
+        title={
+          <h1 className={styles.title}>
+            If you have any queries for us,<br />
+            please contact us at the bottom of the page.
+          </h1>
+        }
+        desc={"We always have the resources to make your story come to life in the best way possible. Please feel free to contact us."}
+        image={"/images/party.jpg"}
+      />
+      <ScrollText />
     </div>
   );
 };

4acd18f4ef47f50d0f094a5468546713553851eb
k_95.21 <kohta0421kochi@gmail.com>
2023-12-02 16:11:50 +0900

    add contact prersonal components

---
+++ src/components/organisms/PersonalForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// scss import
+import styles from './styles/PersonalForm.module.scss';
+
+
+const PersonalForm = () => {
+    return (
+        <div className={styles.container}>
+            <h1 >個人のお客様用お問い合わせフォーム</h1>
+        </div>
+    )
+}
+
+export default PersonalForm

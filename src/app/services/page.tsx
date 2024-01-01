25f799bf2ea4a4cd81771eeebc5bb5fbd639f0b7
k_95.21 <kohta0421kochi@gmail.com>
2024-01-01 11:20:30 +0900

    add new page about service top page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,11 +1,46 @@
 import React from "react";
 
 // scss import
+import styles from './Services.module.scss'
+
+// components import
+import Header from "@/components/organisms/Header";
+import SubPageHeroLayout from "@/components/templates/SubPageHeroLayout";
+import ScrollText from "@/components/atoms/ScrollText";
+import ServicesContent from "./ServicesContent";
+
 
 const Services = () => {
   return (
-    <div>
-      <h1>Services</h1>
+    <div className={styles.services}>
+      <Header />
+      <SubPageHeroLayout
+        id={4}
+        linklevel={
+          [
+            {
+              id: 1,
+              text: "HOME",
+              link: "/"
+            },
+            {
+              id: 2,
+              text: "SERVICES",
+              link: "/services"
+            }
+          ]
+        }
+        title={
+          <h1 className={styles.title}>
+            About the services we can provide
+          </h1>
+        }
+        desc={"We run a number of great, multi-genre communities. Please take a look here."}
+        image={"/images/party.jpg"}
+      />
+      <ScrollText />
+      {/* main content */}
+      <ServicesContent />
     </div>
   );
 };

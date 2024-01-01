6997c319c07fd3ca46e75ad55989d7fcf3a48b81
k_95.21 <kohta0421kochi@gmail.com>
2024-01-01 11:04:30 +0900

    add all community children page and link all button

--- src/app/community/CommunityContent.tsx
+++ src/app/community/CommunityContent.tsx
@@ -25,7 +25,7 @@ const CommunityContent = () => {
                 <div className={styles.titleFlexBox}>
                     <h1 className={styles.titleNumber}>1</h1>
                     <div className={styles.titleRow}>
-                        <h3 className={styles.titleText}>OUTDOOR</h3>
+                        <h3 className={`${styles.titleText} ${styles.yellow}`}>OUTDOOR</h3>
                         <h3 className={styles.mainTitle}>Rover</h3>
                     </div>
                 </div>
@@ -39,6 +39,67 @@ const CommunityContent = () => {
                     />
                     <SubPageMainText id={1} text='山、海、川。ネイチャーアクティビティ好きな若者が集うコミュニティ' />
                     <Link className={styles.link} href="/community/outdoor">View More</Link>
+                </div>
+            </div>
+            {/* 2 */}
+            <div className={styles.community}>
+                <div className={styles.titleFlexBox}>
+                    <h1 className={styles.titleNumber}>2</h1>
+                    <div className={styles.titleRow}>
+                        <h3 className={`${styles.titleText} ${styles.blue}`}>SURFING</h3>
+                        <h3 className={styles.mainTitle}>Gnarlys</h3>
+                    </div>
+                </div>
+                <div className={styles.contentBox}>
+                    <Image
+                        className={styles.image}
+                        src={"/images/about.jpg"}
+                        alt={"rover"}
+                        width={600}
+                        height={400}
+                    />
+                    <SubPageMainText id={1} text='初心者からプロまで。海を愛し、サーフカルチャーを愛する若者のサーフコミュニティ' />
+                    <Link className={styles.link} href="/community/surf">View More</Link>
+                </div>
+            </div>
+            <div className={styles.community}>
+                <div className={styles.titleFlexBox}>
+                    <h1 className={styles.titleNumber}>3</h1>
+                    <div className={styles.titleRow}>
+                        <h3 className={`${styles.titleText} ${styles.green}`}>ENVIRONMENT</h3>
+                        <h3 className={styles.mainTitle}>VYBE</h3>
+                    </div>
+                </div>
+                <div className={styles.contentBox}>
+                    <Image
+                        className={styles.image}
+                        src={"/images/about.jpg"}
+                        alt={"rover"}
+                        width={600}
+                        height={400}
+                    />
+                    <SubPageMainText id={1} text='山、海、川。ネイチャーアクティビティ好きな若者が集うコミュニティ' />
+                    <Link className={styles.link} href="/community/vybe">View More</Link>
+                </div>
+            </div>
+            <div className={styles.community}>
+                <div className={styles.titleFlexBox}>
+                    <h1 className={styles.titleNumber}>4</h1>
+                    <div className={styles.titleRow}>
+                        <h3 className={`${styles.titleText} ${styles.red}`}>EVENT</h3>
+                        <h3 className={styles.mainTitle}>Luten</h3>
+                    </div>
+                </div>
+                <div className={styles.contentBox}>
+                    <Image
+                        className={styles.image}
+                        src={"/images/about.jpg"}
+                        alt={"rover"}
+                        width={600}
+                        height={400}
+                    />
+                    <SubPageMainText id={1} text='新進気鋭でcoolな若者がプロデュース。時代とクリエイティブを意識したイベントチーム' />
+                    <Link className={styles.link} href="/community/event">View More</Link>
                 </div>
             </div>
 

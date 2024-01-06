d7e002009475cf5b7fa04260402eb9c17b828c32
k_95.21 <kohta0421kochi@gmail.com>
2024-01-06 20:00:14 +0900

    add new aos scroll animation #50

--- src/components/molecules/HeroTitle.tsx
+++ src/components/molecules/HeroTitle.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
+
+// aos imports
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // scss import
 import styles from "./styles/HeroTitle.module.scss";
 
 const HeroTitle = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // アニメーションの持続時間
+      once: true, // アニメーションを一度だけ実行する
+    });
+  }, []);
   return (
     <div className={styles.heroBox}>
-      <p className={styles.subJaTitle}>今までにない若者のコミュニティ</p>
-      <h1 className={styles.title}>Creativity</h1>
+      <p className={styles.subJaTitle}  data-aos="fade-up">今までにない若者のコミュニティ</p>
+      <h1 className={styles.title} data-aos="fade-up">Creativity</h1>
       <div className={styles.flexBox}>
-        <h1 className={styles.centerTitle}>Energy</h1>
-        <p className={styles.textBoxText}>
+        <h1 className={styles.centerTitle} data-aos="fade-up">Energy</h1>
+        <p className={styles.textBoxText} data-aos="fade-up">
           A HUB FOSTERING THE DYNAMIC AND UNIQUE CREATIVITY OF YOUTH, OFFERING DIVERSE EVENTS BASED IN TOKYO
         </p>
       </div>
-      <h1 className={styles.title}>Synergy</h1>
+      <h1 className={styles.title} data-aos="fade-up">Synergy</h1>
     </div>
   );
 };
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
+
+// aos imports
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // scss import
 import styles from "./styles/Header.module.scss";
@@ -7,8 +12,14 @@ import styles from "./styles/Header.module.scss";
 import Logo from "../atoms/Logo";
 
 const Header = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // アニメーションの持続時間
+      once: true, // アニメーションを一度だけ実行する
+    });
+  }, []);
   return (
-    <div className={styles.header}>
+    <div className={styles.header} data-aos="fade-down">
       <div className={styles.container}>
         <Logo id={1} link="/" size="medium" color="white" />
       </div>

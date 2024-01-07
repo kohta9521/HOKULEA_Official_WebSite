fb5393ed95e5f94ab9ae3d5a0787ee8b198ab4c8
k_95.21 <kohta0421kochi@gmail.com>
2024-01-07 23:55:08 +0900

    add new contact button

--- src/components/organisms/PersonalForm.tsx
+++ src/components/organisms/PersonalForm.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Link from 'next/link';
+
 
 // scss import
 import styles from './styles/PersonalForm.module.scss';
@@ -8,6 +10,11 @@ const PersonalForm = () => {
     return (
         <div className={styles.container}>
             <h1 >個人のお客様用お問い合わせフォーム</h1>
+            <div className={styles.buttonBox}>
+                <Link href={"https://forms.gle/cj2GatJSmfiWFqiJ7"} className={styles.button}>
+                    フォームはこちらから
+                </Link>
+            </div>
         </div>
     )
 }

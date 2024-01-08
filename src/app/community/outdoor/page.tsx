70faf5cd1e12bdce376edaaa47b5abb34060a099
k_95.21 <kohta0421kochi@gmail.com>
2024-01-08 16:43:08 +0900

    add new hero section atomic desing

--- src/app/community/outdoor/page.tsx
+++ src/app/community/outdoor/page.tsx
@@ -5,11 +5,20 @@ import styles from './Outdoor.module.scss'
 
 // components import
 import Header from "@/components/organisms/Header";
+import SubChildPage from "@/components/templates/SubChildPage";
 
 const Outdoor = () => {
   return (
     <div className={styles.outdoor}>
       <Header />
+      <SubChildPage
+        id={1}
+        subTitle="サブタイトル"
+        title="タイトル"
+        desc="sample your text in darbusu"
+      >
+        <h1>hello</h1>
+      </SubChildPage>
     </div>
   );
 };
--- src/components/templates/SubChildPage.tsx
+++ src/components/templates/SubChildPage.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-
+import Image from 'next/image';
 
 // scss ipmort
 import styles from './styles/SubChildpage.module.scss'
 
 
+// components import
+
+
+
 // props type
 export type SubChildPageProps = {
     id: number
+    subTitle: string
+    title: string
+    desc: string
     children: React.ReactNode
 }
 
@@ -15,11 +22,32 @@ export type SubChildPageProps = {
 
 const SubChildPage = ({
     id,
+    subTitle,
+    title,
+    desc,
     children
 }: SubChildPageProps) => {
     return (
         <div className={styles.subChildpage} key={id}>
-            {children}
+            <div className={styles.hero}>
+                <div className={styles.leftBox}>
+                    <p className={styles.subTitle}>
+                        {subTitle}
+                    </p>
+                    <h1 className={styles.title}>
+                        {title}
+                    </h1>
+                    <p className={styles.desc}>
+                        {desc}
+                    </p>
+                </div>
+                <div className={styles.rightBox}>
+                    <Image className={styles.imageItem} src={"/images/about.jpg"} width={1000} height={800} alt='hero部分の画像' />
+                </div>
+            </div>
+            <div className={styles.body}>
+                {children}
+            </div>
         </div>
     )
 }

a19a51a3818601d8aed90f344b2ea3144f18840c
k_95.21 <kohta0421kochi@gmail.com>
2024-01-08 17:11:29 +0900

    add new suchildpge components responsive design

--- src/components/templates/SubChildPage.tsx
+++ src/components/templates/SubChildPage.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+'use client'
+import React, { useEffect } from "react";
 import Image from 'next/image';
 
 // scss ipmort
 import styles from './styles/SubChildpage.module.scss'
+import SubPageBody from './SubPageBody';
+
+// aos imports
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 // components import
@@ -27,27 +33,33 @@ const SubChildPage = ({
     desc,
     children
 }: SubChildPageProps) => {
+    useEffect(() => {
+        AOS.init({
+          duration: 1000, // アニメーションの持続時間
+          once: true, // アニメーションを一度だけ実行する
+        });
+    }, []);
     return (
-        <div className={styles.subChildpage} key={id}>
+        <div className={styles.subChildPage} key={id}>
             <div className={styles.hero}>
                 <div className={styles.leftBox}>
-                    <p className={styles.subTitle}>
+                    <p className={styles.subTitle} data-aos="fade-right">
                         {subTitle}
                     </p>
-                    <h1 className={styles.title}>
+                    <h1 className={styles.title} data-aos="fade-right">
                         {title}
                     </h1>
                     <p className={styles.desc}>
                         {desc}
                     </p>
                 </div>
-                <div className={styles.rightBox}>
+                <div className={styles.rightBox} data-auto="fade-left">
                     <Image className={styles.imageItem} src={"/images/about.jpg"} width={1000} height={800} alt='hero部分の画像' />
                 </div>
             </div>
-            <div className={styles.body}>
+            <SubPageBody id={1}>
                 {children}
-            </div>
+            </SubPageBody>
         </div>
     )
 }

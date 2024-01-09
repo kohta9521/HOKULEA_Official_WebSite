929b87f229983ff2f3d7c08c39269400e997837f
k_95.21 <kohta0421kochi@gmail.com>
2024-01-09 17:21:09 +0900

    add new surfing community child page

--- src/app/community/surf/page.tsx
+++ src/app/community/surf/page.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
 // scss import
+import styles from './Surf.module.scss';
+
+// components import
+import Header from "@/components/organisms/Header";
+import SubChildPage from "@/components/templates/SubChildPage";
+
 
 const Surf = () => {
   return (
-    <div>
-      <h1>Surf</h1>
+    <div className={styles.surf}>
+      <Header />
+      <SubChildPage
+        id={2}
+        subTitle="SURFING"
+        title="Gnarlys"
+        desc="surfing community"
+      >
+        {/* man content */}
+        <h1>sample</h1>
+      </SubChildPage>
     </div>
   );
-};
+};  
 
 export default Surf;

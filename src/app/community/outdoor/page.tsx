e08b35f4d317f969b16adc6d742adabe009aec2e
k_95.21 <kohta0421kochi@gmail.com>
2024-01-12 14:26:56 +0900

    add new vybe child page

--- src/app/community/outdoor/page.tsx
+++ src/app/community/outdoor/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 // scss import
-import styles from './Outdoor.module.scss'
+import styles from "./Outdoor.module.scss";
 
 // components import
 import Header from "@/components/organisms/Header";
@@ -20,6 +20,7 @@ const Outdoor = () => {
         id={1}
         subTitle="OUTDOOR"
         title="ROVER"
+        img="/images/outdoor.png"
         desc="We don't just end up playing activities as friends. Outdoor Community"
       >
         {/* main content */}
@@ -28,9 +29,18 @@ const Outdoor = () => {
           subTitle="We are always waiting to hear from you. Let's create something together."
           mainTitle="About Rover..."
         />
-        <SubPageMainText id={1} text="Camping, Trekking, Snowboarding, SUP、、、" />
-        <SubPageMainText id={1} text="HOKULEAアウトドアアクティビティのコミュニティ。このコミュニティの強みは、楽しかったで終わらせない、その場所の楽しさを波及し魅せていくこと。なぜならば自然が好きだから。もっと自然の魅力に気がついて欲しい。そんな自然を愛する若者が様々なアウトドアアクティビティを行っています。" />
-        <SubPageMainText id={1} text="アウトドアに知見や関心が多いcoolな若者が自然と対峙し素敵な時間と思い出を作っています。" />
+        <SubPageMainText
+          id={1}
+          text="Camping, Trekking, Snowboarding, SUP、、、"
+        />
+        <SubPageMainText
+          id={1}
+          text="HOKULEAアウトドアアクティビティのコミュニティ。このコミュニティの強みは、楽しかったで終わらせない、その場所の楽しさを波及し魅せていくこと。なぜならば自然が好きだから。もっと自然の魅力に気がついて欲しい。そんな自然を愛する若者が様々なアウトドアアクティビティを行っています。"
+        />
+        <SubPageMainText
+          id={1}
+          text="アウトドアに知見や関心が多いcoolな若者が自然と対峙し素敵な時間と思い出を作っています。"
+        />
         <SubPageInsertImage
           id={1}
           image={"/images/about.jpg"}
@@ -41,31 +51,34 @@ const Outdoor = () => {
           subTitle="We are always waiting to hear from you. Let's create something together."
           mainTitle="SERVICE CHARGE"
         />
-        <SubPageSubSecTitle
-          id={0}
-          text="Event Produce"
-        />
+        <SubPageSubSecTitle id={0} text="Event Produce" />
         <SubPageMainText id={1} text="Coolな若者とともにアウトドア体験。" />
-        <SubPageMainText id={1} text="場所決め、工程作り、場所の許可取りや備品の手配、アウトドア保険、先方のプロダクトを身につけ、アウトドア体験を行います。 エリアやイベントの規模、参加人数は要相談。" />
-        <SpaceBox id={1} level="small" />
-        <SubPageSubSecTitle
-          id={0}
-          text="SNS PR"
+        <SubPageMainText
+          id={1}
+          text="場所決め、工程作り、場所の許可取りや備品の手配、アウトドア保険、先方のプロダクトを身につけ、アウトドア体験を行います。 エリアやイベントの規模、参加人数は要相談。"
         />
-        <SubPageMainText id={1} text="Instagramにて案件の投稿制作、発信を行います。" />
-        <SubPageMainText id={1} text="オプションにてメンバーの個人アカウントでも発信を行います。" />
         <SpaceBox id={1} level="small" />
-        <SubPageSubSecTitle
-          id={0}
-          text="Products Produce"
+        <SubPageSubSecTitle id={0} text="SNS PR" />
+        <SubPageMainText
+          id={1}
+          text="Instagramにて案件の投稿制作、発信を行います。"
         />
-        <SubPageMainText id={1} text="若者の感性を元に今の時代にあったプロダクトの制作に支援いたします。" />
+        <SubPageMainText
+          id={1}
+          text="オプションにてメンバーの個人アカウントでも発信を行います。"
+        />
         <SpaceBox id={1} level="small" />
-        <SubPageSubSecTitle
-          id={0}
-          text="Outdoor Consulting"
+        <SubPageSubSecTitle id={0} text="Products Produce" />
+        <SubPageMainText
+          id={1}
+          text="若者の感性を元に今の時代にあったプロダクトの制作に支援いたします。"
         />
-        <SubPageMainText id={1} text="今の時代を生き、かつアウトドアの知見あるクリエイティブな若者の考えや目線を伝え向上と指針を示す援助をいたします。" />
+        <SpaceBox id={1} level="small" />
+        <SubPageSubSecTitle id={0} text="Outdoor Consulting" />
+        <SubPageMainText
+          id={1}
+          text="今の時代を生き、かつアウトドアの知見あるクリエイティブな若者の考えや目線を伝え向上と指針を示す援助をいたします。"
+        />
         <SpaceBox id={1} level="small" />
         <SubPageSecTitle
           id={7}
